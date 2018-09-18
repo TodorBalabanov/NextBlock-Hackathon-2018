13074fa641a0ad6b3ebc9146eb88289e7e39a2a1
Todor Balabanov <todor.balabanov@gmail.com>
2018-09-18 10:26:07 +0300

    Not final.

--- Main.java
+++ Main.java
@@ -1,4 +1,24 @@
+import java.io.IOException;
+
+import org.json.JSONObject;
+import org.json.JSONException;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.client.ClientProtocolException;
+
 public class Main{
-	public static void main(String args[]) {
+	public static void main(String args[]) throws ClientProtocolException, IOException {
+		HttpClient client = new DefaultHttpClient();
+		client.getParams().setParameter("http.protocol.content-charset", "UTF-8");
+		HttpPost post = new HttpPost("https://api.kraken.com/0/public/OHLC?pair=xxbtzeur&interval=21600");
+
+		HttpResponse response = client.execute(post);
+		JSONObject result = new JSONObject(EntityUtils.toString(response.getEntity(), "UTF-8"));
+
+		System.out.println( result.getJSONObject("result").getJSONArray("XXBTZEUR") );
 	}
 }
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-javac Main.java
-java Main
+javac -cp ./:commons-codec-1.9.jar:httpclient-cache-4.5.2.jar:commons-logging-1.2.jar:httpclient-win-4.5.2.jar:encog-core-3.4.jar:httpcore-4.4.4.jar:fluent-hc-4.5.2.jar:httpmime-4.5.2.jar:httpclient-4.5.2.jar:json-20180813.jar Main.java
+java -cp ./:commons-codec-1.9.jar:httpclient-cache-4.5.2.jar:commons-logging-1.2.jar:httpclient-win-4.5.2.jar:encog-core-3.4.jar:httpcore-4.4.4.jar:fluent-hc-4.5.2.jar:httpmime-4.5.2.jar:httpclient-4.5.2.jar:json-20180813.jar Main
